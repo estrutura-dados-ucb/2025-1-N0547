5fe95388022ebaa053203bf82e40c2b83b076500
joaorobson <joaorobsonmartins@gmail.com>
2025-04-22 21:36:22 -0300

    Aula 7

---
+++ aula-7-listas-encadeadas/listas_encadeadas/filmes.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "filmes.h"
+
+
+Filme *inicio;
+
+void criaLista(Filme *filme){
+    inicio = filme;
+    inicio->prox = NULL;
+}
+
+
+void adicionarFilme(Filme *filme){
+    if(inicio == NULL){
+        criaLista(filme);
+    } else {
+        filme->prox = inicio;
+        inicio = filme;
+    }
+}
+
+void listarFilmes(){
+    printf("Listando filmes...\n");
+    Filme *filmeAtual = inicio;
+
+    while(filmeAtual != NULL){
+        printf("nome do filme: %s\n", filmeAtual->nome);
+        printf("nome do filme: %d\n", filmeAtual->anoLancamento);
+        printf("nome do filme: %s\n", filmeAtual->diretor);
+        printf("===================================\n");
+        filmeAtual = filmeAtual->prox;
+    }
+}
+
+Filme* buscarFilme(char* nomeBusca){
+    Filme *filmeAtual = inicio;
+
+    while(filmeAtual != NULL){
+        if(strcmp(filmeAtual->nome, nomeBusca) == 0){
+            printf("Filme encontrado!\n");
+            return filmeAtual;
+        }
+        filmeAtual = filmeAtual->prox;
+    }
+    return NULL;
+
+}
+
+void removerFilme(char *nome){
+    printf("Tentando remover %s\n", nome);
+    Filme* filmeAtual = inicio;
+    Filme* proxFilme = filmeAtual->prox;
+
+    // se for o primeiro filme
+    if(strcmp(filmeAtual->nome, nome) == 0){
+        printf("Removendo %s...\n", nome);
+        free(filmeAtual);
+        inicio = proxFilme;
+    }
+
+    else {
+        while(proxFilme != NULL){
+            // achei o filme
+            if(strcmp(proxFilme->nome, nome) == 0){
+                printf("Removendo %s...\n", nome);
+                filmeAtual->prox = proxFilme->prox;
+                free(proxFilme);
+                break;
+            }
+
+            filmeAtual = filmeAtual->prox;
+            proxFilme = proxFilme->prox;
+        }
+    }
+}
+
+
+
+int main(){
+    Filme *filme1, *filme2;
+    filme1 = (Filme*) malloc(sizeof(Filme));
+    filme2 = (Filme*) malloc(sizeof(Filme));
+
+    strcpy(filme1->nome, "duna 2");
+    strcpy(filme1->diretor, "denis villenueve");
+    filme1->anoLancamento = 2024;
+
+    strcpy(filme2->nome, "avatar 2");
+    strcpy(filme2->diretor, "james cameron");
+    filme2->anoLancamento = 2023;
+
+    adicionarFilme(filme1);
+    adicionarFilme(filme2);
+
+    listarFilmes();
+
+    Filme *retornoBusca = buscarFilme("duna 2");
+    if(retornoBusca) {
+        printf("Filme encontrado: %s\n", retornoBusca->nome);
+    } else {
+        printf("Filme nao encontrado!\n");
+    }
+
+    removerFilme("duna 2");
+
+    listarFilmes();
+
+    return 0;
+}
---
+++ aula-7-listas-encadeadas/listas_encadeadas/filmes.h
@@ -0,0 +1,19 @@
+#ifndef FILMES_H
+#define FILMES_H
+
+typedef struct Filme {
+    char nome[50];
+    int anoLancamento;
+    char diretor[50];
+    struct Filme *prox;
+} Filme;
+
+
+void criaLista();
+void listarFilmes();
+void adicionarFilme(Filme *filmes);
+Filme* buscarFilme(char* nome);
+void removerFilme(char* nome);
+int contarNumFilmes(Filme *filmes);
+
+#endif
